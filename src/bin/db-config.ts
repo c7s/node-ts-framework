f9534c42f797fbee24bc0a6ef86e0b9f425aea98
Mikhail Elfimov <melfimov@changers.team>
2018-02-14 16:43:22 +0300

    PB-3102 microservice boilerplate

--- src/bin/db-config.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import '../bootstrap';
-import { DbConfig } from '@c7s/config';
-import { container } from '../di';
-import { Type } from '../Type';
-
-const dbConfig = container.get<DbConfig>(Type.DbConfig);
-process.stdout.write(JSON.stringify(dbConfig));
